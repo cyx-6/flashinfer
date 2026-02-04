40026317cdbbc9e01631b5757eaae1f05456c6ed
Yaxing Cai <caiyaxing666@gmail.com>
2026-02-04 22:05:39 +0800

    fix

--- r.py
+++ r.py
@@ -1,32 +1,3 @@
-# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# {$nv-internal-release file}
-
 import torch
 import cutlass.cute as cute
 from cutlass.cute.runtime import from_dlpack
@@ -238,11 +209,11 @@ def benchmark_overhead():
 
     # Create fake tensors with symbolic stride for arbitrary stride support
     x_fake = cute.runtime.make_fake_tensor(
-        dtype, (sym_m, H), (sym_row_stride_x, 1), assumed_align=16
+        dtype, (sym_m, hidden_size), (sym_row_stride_x, 1), assumed_align=16
     )
-    w_fake = cute.runtime.make_fake_compact_tensor(dtype, (H,), assumed_align=16)
+    w_fake = cute.runtime.make_fake_compact_tensor(dtype, (hidden_size,), assumed_align=16)
     out_fake = cute.runtime.make_fake_tensor(
-        dtype, (sym_m, H), (sym_row_stride_y, 1), assumed_align=16
+        dtype, (sym_m, hidden_size), (sym_row_stride_y, 1), assumed_align=16
     )
 
     stream_fake = cute.runtime.make_fake_stream(use_tvm_ffi_env_stream=True)
@@ -276,11 +247,11 @@ def benchmark_e2e():
 
         # Create fake tensors with symbolic stride for arbitrary stride support
         x_fake = cute.runtime.make_fake_tensor(
-            dtype, (sym_m, H), (sym_row_stride_x, 1), assumed_align=16
+            dtype, (sym_m, hidden_size), (sym_row_stride_x, 1), assumed_align=16
         )
-        w_fake = cute.runtime.make_fake_compact_tensor(dtype, (H,), assumed_align=16)
+        w_fake = cute.runtime.make_fake_compact_tensor(dtype, (hidden_size,), assumed_align=16)
         out_fake = cute.runtime.make_fake_tensor(
-            dtype, (sym_m, H), (sym_row_stride_y, 1), assumed_align=16
+            dtype, (sym_m, hidden_size), (sym_row_stride_y, 1), assumed_align=16
         )
 
         stream_fake = cute.runtime.make_fake_stream(use_tvm_ffi_env_stream=True)
